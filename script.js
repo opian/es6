509a026bb427db036ebd359c296c99d604ac9afe
opian <bekriya@gmail.com>
2017-12-25 21:40:09 +0300

    lo

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 window.onload = function () {
 //Spread Operator
+	var meats = ['ham', 'salami','bacon'];
+	console.log(meats);
+	console.log(...meats);
+
+	console.log('-------');
+	var nums1 = [1,2,3,4];
+	var nums2 = [5,6,7,8,9];
+	var nums  = [...nums1,...nums2];
+	console.log('-------');
+	console.log(...nums1, 4,3,2,1);
+	console.log(nums);
 }
