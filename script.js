cbccb059cb66133c263d80dfa7a9695447d109a9
opian <bekriya@gmail.com>
2017-12-25 21:50:48 +0300

    log

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 window.onload = function () {
 //Template Sting
-	
+	var temp = `Hi my name is 
+	Ryu`;
+	console.log(temp);
+	function logNinja(name,age) {
+		console.log(`hi my name is ${name} and age is ${age - 10}`);
+	}
+	logNinja('ahmet','32');
 }
