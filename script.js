721ca9d462d63c398870d11ed74c578b6b4e5bb0
opian <bekriya@gmail.com>
2017-12-25 15:00:55 +0300

    deneme

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 window.onload = function () {
-	const pi = 3.14;
-	console.log('outside pi: ' + pi);
-	function calcArea(r) {
-		const pi = 10;
-		console.log('inside pi: ' + pi);
-
-		console.log('alan: ' + pi * r * r);
+	var x = 10;
+	if (x>5) {
+		var x = 5;
 	}
-
-	calcArea(3);
 }
