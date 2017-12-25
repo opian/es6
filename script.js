c73058bc481cb78591fffe92716b3ff5dac6c6f1
opian <bekriya@gmail.com>
2017-12-25 15:12:02 +0300

    lo

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 window.onload = function () {
 	var x = 10;
+	var y = 20;
 	if (x > 5) {
 		let x = 5;
+		var y = 40;
 		console.log('x inside: ' + x);
+		console.log('y inside: ' + y);
 	}
 	console.log('x outside: ' + x);
+	console.log('y outside: ' + y);
 }
