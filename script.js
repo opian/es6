5c45449b39b47185433f512192a614df884a7fe3
opian <bekriya@gmail.com>
2017-12-25 21:25:35 +0300

    lo

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 window.onload = function () {
+	// default parameters
+
+	function log(num=10) {
+		console.log(num);
+	}
+	log();
+
 	/*
-	var x = 10;
-	var y = 20;
-	if (x > 5) {
-		let x = 5;
-		var y = 40;
-		console.log('x inside: ' + x);
-		console.log('y inside: ' + y);
-	}
-	console.log('x outside: ' + x);
-	console.log('y outside: ' + y);
+		yukaridaki kullanimda log() fonksiyonuna deger
+		gelirse, gelen degeri, gelmezse 10 degerini yazacak.
+
 	*/
-	var items = document.getElementsByTagName('li');
-	console.log(items);
-	for (let i = 0; i < items.length; i++) {
-		items[i].onclick = function(){
-			console.log(i);
-			//i degiskeni var keywrd ile tanimlandiginda 
-			//dingu en bastan kuruldugu icin ekrAna
-			//hep 4 yaziyur.
-		}
+
+	function logNinja(name="Ryu", belt = "Black", age = "23"){
+		console.log('Hi my names '+name+' and my belt is: '+belt+
+		' and my age is: '+age+'');
 	}
+	logNinja();
+	logNinja('ahmet','kirmizi','32');
 }
