dbccd369c63b819bda2989c7ddb19e4c42e08eb4
opian <bekriya@gmail.com>
2017-12-25 22:55:06 +0300

    lo

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 window.onload = function () {
-//Template Sting
-	var temp = `Hi my name is 
-	Ryu`;
-	console.log(temp);
-	function logNinja(name,age) {
-		console.log(`hi my name is ${name} and age is ${age - 10}`);
-	}
-	logNinja('ahmet','32');
+// New String Methods
+	//repeat
+	//startsWith
+	//endsWith
+	//includes 
+	var mesaj = `Lorem impsum dolor sit amed`;
+	console.log( mesaj.repeat(5));
+	console.log(mesaj.startsWith('Lore'));
+	console.log('startsWith baslangicNoktasi');
+	console.log(mesaj.startsWith('sum',9));
+	console.log('endsWith')
+	console.log(mesaj.endsWith('amed'));
+	console.log('endsWith bitisNoktasi')
+	console.log(mesaj.endsWith('sit', mesaj.length-5));
+	console.log('include')
+	console.log(mesaj.includes('impsum'));
+
 }
