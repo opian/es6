68cfd3f34268b6f145adf3f4fceece7288219eda
opian <bekriya@gmail.com>
2017-12-25 21:27:36 +0300

    log

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
 window.onload = function () {
-	// default parameters
-
-	function log(num=10) {
-		console.log(num);
-	}
-	log();
-
-	/*
-		yukaridaki kullanimda log() fonksiyonuna deger
-		gelirse, gelen degeri, gelmezse 10 degerini yazacak.
-
-	*/
-
-	function logNinja(name="Ryu", belt = "Black", age = "23"){
-		console.log('Hi my names '+name+' and my belt is: '+belt+
-		' and my age is: '+age+'');
-	}
-	logNinja();
-	logNinja('ahmet','kirmizi','32');
+//Spread Operator
 }
